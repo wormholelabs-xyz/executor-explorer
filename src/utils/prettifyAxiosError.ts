e466af45246d9dee3fc82aa86fdf752417e5af9d
Priyajeet Hora <priyajeet@gmail.com>
2025-04-23 08:58:36 -0700

    Account for error objects and not just strings

--- src/utils/prettifyAxiosError.ts
+++ src/utils/prettifyAxiosError.ts
@@ -1,10 +1,12 @@
 export default function prettifyAxiosError(e: any) {
   const responseData = e?.response?.data;
   const errMsg = e?.message;
-  if (responseData && errMsg) {
-    return `${errMsg}: ${responseData}`;
-  } else if (responseData) {
-    return responseData;
+  const errDetails = responseData?.message || responseData;
+
+  if (errDetails && errMsg) {
+    return `${errMsg}: ${errDetails}`;
+  } else if (errDetails) {
+    return errDetails;
   } else if (errMsg) {
     return errMsg;
   } else {
