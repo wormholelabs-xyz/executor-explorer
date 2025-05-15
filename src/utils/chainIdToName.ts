5901ddae03cfefeaf0093d964bda49eaee5b1bfd
Evan Gray <battledingo@gmail.com>
2025-05-15 11:59:30 -0400

    explorer links for new chains

--- src/utils/chainIdToName.ts
+++ src/utils/chainIdToName.ts
@@ -1,6 +1,11 @@
 import { toChain } from "@wormhole-foundation/sdk-base";
+import { CHAIN_ID_SONIC } from "./consts";
 
 export const chainIdToName = (chainId: number) => {
+  // TODO: remove once Sonic is included in the Wormhole SDK
+  if (chainId === CHAIN_ID_SONIC) {
+    return `Sonic (${chainId})`;
+  }
   try {
     return `${toChain(chainId)} (${chainId})`;
   } catch (e) {
