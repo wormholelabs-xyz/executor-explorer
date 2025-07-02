577bc0f48aa24ccc59a92ad7acb0918603a25d2b
Douglas Galico <douglas.galico@gmail.com>
2025-07-02 16:20:03 +0200

    add: Plume & Mezo

--- src/utils/chainIdToName.ts
+++ src/utils/chainIdToName.ts
@@ -1,12 +1,15 @@
 import { toChain } from "@wormhole-foundation/sdk-base";
-import { CHAIN_ID_CODEX, CHAIN_ID_FOGO } from "./consts";
+import { CHAIN_ID_CODEX, CHAIN_ID_MEZO, CHAIN_ID_PLUME } from "./consts";
 
 export const chainIdToName = (chainId: number) => {
-  if (chainId === CHAIN_ID_FOGO) {
-    return `Fogo (${chainId})`;
-  }
   if (chainId === CHAIN_ID_CODEX) {
     return `Codex (${chainId})`;
+  }
+  if (chainId === CHAIN_ID_MEZO) {
+    return `Mezo (${chainId})`;
+  }
+  if (chainId === CHAIN_ID_PLUME) {
+    return `Plume (${chainId})`;
   }
   try {
     return `${toChain(chainId)} (${chainId})`;
--- src/utils/consts.ts
+++ src/utils/consts.ts
@@ -1,4 +1,7 @@
-// https://github.com/wormhole-foundation/wormhole/blob/main/sdk/vaa/structs.go#L675
-export const CHAIN_ID_FOGO = 51;
-
 export const CHAIN_ID_CODEX = 54;
+
+// https://github.com/wormhole-foundation/wormhole/blob/ac82e17eb67023931229a7f5a8b2266fd63c0fef/sdk/vaa/structs.go#L667
+export const CHAIN_ID_MEZO = 50;
+
+// https://github.com/wormhole-foundation/wormhole/blob/08d600253fc90800cbf6872f450b8b92d9cf49e3/sdk/vaa/structs.go#L688
+export const CHAIN_ID_PLUME = 55;
